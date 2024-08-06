3651640f8e88dc86deef1eb942f33b27e4535563
VicTech444 <Victor_verq@outlook.com>
2024-08-06 16:02:20 -0500

    add enviroment variable from hosting and andd .env to ignore

--- src/database/mongodb.js
+++ src/database/mongodb.js
@@ -2,7 +2,7 @@ import { MongoClient, ServerApiVersion } from 'mongodb'
 import 'dotenv/config'
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(process.env.MONGO_STRING_CONNECTION, {
+const client = new MongoClient(process.env.MONGODB_URI, {
   serverApi: {
     version: ServerApiVersion.v1,
     strict: true,
