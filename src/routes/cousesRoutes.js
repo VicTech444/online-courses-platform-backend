40dd23200480381e951c1812dc040844cd4ce4f1
VicTech444 <Victor_verq@outlook.com>
2024-08-06 15:46:08 -0500

    improve course route performance with db

--- src/routes/cousesRoutes.js
+++ src/routes/cousesRoutes.js
@@ -3,6 +3,19 @@ import { runDB } from "../database/mongodb.js";
 
 export const coursesRouter = e.Router();
 
-coursesRouter.get('/showAllCourses', (req, res) => {
-    res.send({message: "This should be the route to show all courses"});
-})
+coursesRouter.get('/showAllCourses', async (req, res) => {
+    let mongoClient;
+    try {
+      mongoClient = await runDB();
+      let db = mongoClient.db('online_learning_platform');
+      console.log(db)
+      let collections = await db.collection('courses').find().toArray();
+      res.send({ Courses: collections });
+    } catch (error) {
+      console.error('Error connecting to the database:', error);
+      res.status(500).send({ error: 'Error connecting to the database' });
+    } finally {
+      if (mongoClient) {
+        await mongoClient.close(true);
+      }
+    }})
