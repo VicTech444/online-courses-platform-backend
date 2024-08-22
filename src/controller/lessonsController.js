bb71417a86353c8717cdee9a51e3a2d94092925e
VicTech444 <Victor_verq@outlook.com>
2024-08-22 15:43:52 -0500

    fix headers already sent error

--- src/controller/lessonsController.js
+++ src/controller/lessonsController.js
@@ -3,11 +3,11 @@ import { lessonsModel } from "../model/lessonsModel.js";
 export class lessonsController {
     static async getLesson (req, res) {
         let {courseName} = req.body;
-        console.log(courseName)
+
         let response = await lessonsModel.getLesson(courseName);
 
         if (!response) {
-            res.status(500).send({ error: 'Error connecting to the database' });
+            return res.status(500).send({ error: 'Error connecting to the database' });
         }
 
         res.status(200).send({courses: response});
